dd9f51f5f2e953c7b1be8adcc5e15276288bda26
Frederik Aalund <fpa@sbtinstruments.com>
2021-05-13 21:52:45 +0200

    build: remove and ignore stress.sh

--- stress.sh
+++
@@ -1,4 +0,0 @@
-set -e
-for i in {1..100}; do
-    python -m stork reset-device fw-3.0.0-and-sw-2.5.5.swu --device-type bactobox --branding sbt --hostname bb2045014 --jtag-usb-serial 210299AFB3C5
-done
