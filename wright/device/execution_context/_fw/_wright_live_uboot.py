d1b3d57a4712894f0f9c406dd7580c549455dc00
Frederik Aalund <fpa@sbtinstruments.com>
2021-12-15 10:49:31 +0100

    feat: change uhubctl invocation to match yocto-based build

--- wright/device/execution_context/_fw/_wright_live_uboot.py
+++ wright/device/execution_context/_fw/_wright_live_uboot.py
@@ -151,7 +151,9 @@ async def _start_server(
 
 
 async def _power_cycle_usb_ports(*, logger: Optional[Logger] = None) -> None:
-    command = "uhubctl", "--ports", "1", "--action", "cycle"
+    # TODO: Make the search term configurable. Fow now, we rely on the fact that
+    # we use a USB-JTAG adapter from "Digilent Digital".
+    command = "uhubctl", "--search", "Digilent", "--action", "cycle"
     await run_process(command, check_rc=True, stdout_logger=logger)
     # Wait a moment for the USB devices to set themselves up
     await anyio.sleep(2)
